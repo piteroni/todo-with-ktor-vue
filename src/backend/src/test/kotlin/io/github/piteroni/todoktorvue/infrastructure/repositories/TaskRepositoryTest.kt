8d99663c3c619c6ad5e1549357ca8bebfb76a905
WatanabeJunna <dev.piteroni@gmail.com>
2021-06-28 21:53:50 +0900

    更新

--- src/backend/src/test/kotlin/io/github/piteroni/todoktorvue/infrastructure/repositories/TaskRepositoryTest.kt
+++ src/backend/src/test/kotlin/io/github/piteroni/todoktorvue/infrastructure/repositories/TaskRepositoryTest.kt
@@ -6,25 +6,28 @@ import io.github.piteroni.todoktorvue.app.domain.task.TaskName
 import io.github.piteroni.todoktorvue.app.infrastructure.repositories.TaskRepository
 import io.github.piteroni.todoktorvue.testing.factories.UserDataSourceFactory
 import io.github.piteroni.todoktorvue.testing.setUp
+import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
 
-class TaskRepositoryTest : FreeSpec({
-    val taskRepository = TaskRepository()
+class TaskRepositoryTest : FreeSpec() {
+    private val taskRepository = TaskRepository()
 
-    "Saving task" - {
-        "When an unregistered task is passed, task can be registered in the repository" - {
-            val userDataSource = UserDataSourceFactory.make()
-            val task = Task(TaskId.unregistered(), userDataSource.asUser().id, TaskName("new-task"))
-
-            val createdTask = taskRepository.save(task)
-
-            createdTask.name shouldBe TaskName("new-task")
-        }
-    }
-}) {
-    init {
+    override fun beforeSpec(spec: Spec) {
         setUp()
+    }
+
+    init {
+        "Saving task" - {
+            "When an unregistered task is passed, task can be registered in the repository" - {
+                val userDataSource = UserDataSourceFactory.make()
+                val task = Task(TaskId.unregistered(), userDataSource.asUser().id, TaskName("new-task"))
+
+                val createdTask = taskRepository.save(task)
+
+                createdTask.name shouldBe TaskName("new-task")
+            }
+        }
     }
 }
